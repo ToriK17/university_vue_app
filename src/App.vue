92d3b8122f1f1aec39d93eed16f99f724601df08
ToriK17 <vfcad@protonmail.com>
2019-10-18 16:58:00 -0700

    adds authentication

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/signup">Signup</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/logout">Logout</router-link> 
     </div>
     <router-view/>
   </div>
@@ -17,16 +20,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
